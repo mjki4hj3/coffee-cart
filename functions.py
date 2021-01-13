222b9bafe662fa4efc3d4ab2d3578d174190a7bd
Hudeyfa Jama <hudeyfa.jama@gmail.com>
2021-01-13 00:48:48 +0000

    moved all the helper functions to a separate file for cleaner code

---
+++ functions.py
@@ -0,0 +1,43 @@
+import os
+from time import sleep
+
+
+def clr():
+    # Clears the terminal - at some point we can use the clear function from the os module
+    print("\n"*100)
+
+
+def show_products():
+    print("===Under Construction===\n")
+    sleep(1)
+    clr()
+    main_menu()
+
+
+def create_product():
+    print("===Under Construction===\n")
+    sleep(2)
+    clr()
+    main_menu()
+
+
+def update_product():
+    print("===Under Construction===\n")
+    sleep(2)
+    clr()
+    main_menu()
+
+
+def delete_product():
+    print("===Under Construction===\n")
+    sleep(2)
+    clr()
+    main_menu()
+
+
+def exit_app():
+    clr()
+    print("Goodbye")
+    sleep(2)
+    clr()
+    exit()
